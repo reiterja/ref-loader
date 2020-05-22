5feb7bb300dadd26b006ff0748a157079938abf6
Jacob <jacob.reiter@sosi.com>
2020-05-22 12:34:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ module.exports = function(src, map, meta) {
   const callback = this.async();
   try {
     const parsed = parse(src);
-    callback(null, product, map, meta);
+    callback(null, parsed, map, meta);
   } catch (e) {
     callback(e);
   }
 };
+
+// options: {
+//   replacements: [
+//       {
+//           pattern: /\(img\/(.+).png\)/gi, // https://regex101.com/r/O5cs0N/1
+//           replace: (match, p1) => `@ref(img/${p1}.png)`,
+//       },
+//   ],
+// },
